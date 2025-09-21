6422fcb6cabf21a479a2517f11a978f97d635d6a
Amanullah-B <bamanu4@gmail.com>
2025-09-21 11:26:16 +0000

    Adding helper function

--- list_read_access.sh
+++ list_read_access.sh
@@ -7,15 +7,31 @@
 #Version: 1
 ##############
 #Usage: ./list_read_access.sh <owner> <repo> <username> <token>
+
+#Helper function to check arguments
+helper() {
+    expected_cmd_args=4
+    if [[ $# -lt $expected_cmd_args ]];then
+    echo "Please execute the script with required cmd args"
+    echo "#Usage: ./list_read_access.sh <owner> <repo> <username> <token>"
+    exit 1
+    fi
+}
+
+#Call helper function at the script
+helper "$@"
+
+#Assign arguments
 REPO_OWNER=$1
 REPO_NAME=$2
 USERNAME=$3
 TOKEN=$4
 
+
 API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
 #Get collaborators list
-response=$(curl -s -u "${USERNAME}:${TOKEn}" "$API_URL")
+response=$(curl -s -u "${USERNAME}:${TOKEN}" "$API_URL")
 
 #Check if error
 if echo "$response" | grep -q '"message":'; then
@@ -25,3 +41,4 @@ fi
 
 #Extract users with read (pull) access
 echo "$response" | jq -r '.[] | select(.permissions.pull == true) | .login'
+
