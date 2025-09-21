29fca187291c01c56fa6124d898132413b74782e
Amanullah-B <bamanu4@gmail.com>
2025-09-21 10:50:02 +0000

    Shell script to get the list of users who have read access to this Repo

---
+++ list_read_access.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Usage: ./list_read_access.sh <owner> <repo> <username> <token>
+REPO_OWNER=$1
+REPO_NAME=$2
+USERNAME=$3
+TOKEN=$4
+
+API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+#Get collaborators list
+response=$(curl -s -u "${USERNAME}:${TOKEn}" "$API_URL")
+
+#Check if error
+if echo "$response" | grep -q '"message":'; then
+    echo "Error: $(echo "$response" | jq -r '.message')"
+    exit 1
+fi
+
+#Extract users with read (pull) access
+echo "$response" | jq -r '.[] | select(.permissions.pull == true) | .login'
